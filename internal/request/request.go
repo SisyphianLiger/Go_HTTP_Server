755084307e70caa73677dde9235a8f3825d482b9
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2025-03-24 21:15:02 +0100

    Added A HTTP Parser with Tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,93 @@
 package request
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"unicode"
+
+)
+
+type Request struct {
+	RequestLine RequestLine 
+}
+
+type RequestLine struct {
+	HttpVersion string
+	RequestTarget string
+	Method	string
+}
+
+func verifyHttpVersionIsUpper(httpVersion string) (string,error) {
+	
+	if httpVersion == "" {
+		return "",fmt.Errorf("The String is Empty\n")
+	}
+
+	for _,c := range httpVersion {
+		if unicode.IsLower(c){
+			return "",fmt.Errorf("The HTTP Version is not UPPERCASE: %s, Incorrect HTTP Version Specification\n", httpVersion)
+		}
+	}
+
+	return httpVersion, nil
+}
+
+func verifySemanticHTTP(http string) (string,error) {
+	
+	if http != "HTTP/1.1" {
+		return "", fmt.Errorf("Not the correct HTTP Version: Found %s", http)
+	}
+
+	HTTPVersion := strings.Split(http, "/")
+
+	return HTTPVersion[1], nil
+}
+
+func parseIntoRequestLine(request string) (RequestLine, error) {
+
+	parsedData := strings.Split(request, "\r\n")
+	requestLines := strings.Split(parsedData[0], " ")	
+
+	if len(requestLines) != 3 {
+		return RequestLine{}, fmt.Errorf("Invalid Format: Require METHOD TARGET HTTPVERSION, Got: %s\n", parsedData[0])
+	}
+
+	requestTarget := requestLines[1]
+
+	method, methodErr := verifyHttpVersionIsUpper(requestLines[0]);
+	if  methodErr != nil {
+		return RequestLine{}, methodErr
+	}
+
+	version, versionErr := verifySemanticHTTP(requestLines[2]);
+	if  versionErr != nil {
+		return RequestLine{}, versionErr	
+	}
+
+	return RequestLine{
+		HttpVersion: version,
+		RequestTarget: requestTarget,
+		Method: method,
+	}, nil 
+}
+
+
+func requestFromReader(reader io.Reader) (*Request, error) {
+	req, reqErr := io.ReadAll(reader)	
+		
+	if reqErr != nil {
+		fmt.Fprintf(os.Stderr, "Error Parsing Data: %v\n", reqErr)
+		os.Exit(1)
+	}
+
+	// discard everything after request line
+	reqData, err := parseIntoRequestLine(string(req))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Request{ reqData }, nil
+	 
+}
--- internal/request/request_test.go
+++ internal/request/request_test.go
@@ -1,11 +1,48 @@
 package request
 
+
 import (
 	"testing"
-
+	"strings"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+
 func TestRequestLineParse(t *testing.T) {
-	assert.Equal(t, "TheTestagen", "TheTestagen")
+	// Test: Good GET Request line
+	r, err := requestFromReader(strings.NewReader("GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good GET Request line with path
+	r, err = requestFromReader(strings.NewReader("GET /coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t, r)
+	assert.Equal(t, "GET", r.RequestLine.Method)
+	assert.Equal(t, "/coffee", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Good POST Request with path
+	r, err = requestFromReader(strings.NewReader("POST /beans HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.NoError(t, err)
+	require.NotNil(t,r)
+	assert.Equal(t, "POST", r.RequestLine.Method)
+	assert.Equal(t, "/beans", r.RequestLine.RequestTarget)
+	assert.Equal(t, "1.1", r.RequestLine.HttpVersion)
+
+	// Test: Invalid number of parts in request line
+	_, err = requestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)		
+	
+	// Test: Methods Out of Order
+	_, err = requestFromReader(strings.NewReader("/coffee HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)		
+	
+	// Test: HTTP Version is not correct
+	_, err = requestFromReader(strings.NewReader("GET /coffee HTTP/1.2\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n"))
+	require.Error(t, err)		
 }
